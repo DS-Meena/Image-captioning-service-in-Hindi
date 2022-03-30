47c51e1e86e8a2bfe3942c13ab4d965790533a0d
28-08-2000 <dharamsinghmeena2000@gmail.com>
2022-03-30 23:57:05 +0530

    importing text vocab inside wsgi only

--- ICS_hindi/wsgi.py
+++ ICS_hindi/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# for the text vocab error
+from static.text_pre_processing import textVocab
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ICS_hindi.settings')
 
 application = get_wsgi_application()
