2f69e5d039ea2fee642af8c9144f8fceb0064934
28-08-2000 <dharamsinghmeena2000@gmail.com>
2022-03-31 14:05:06 +0530

    getting old one configuration wsgi

--- ICS_hindi/wsgi.py
+++ ICS_hindi/wsgi.py
@@ -12,15 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # for the text vocab error
-# from static.text_pre_processing import textVocab
+from static.text_pre_processing import textVocab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ICS_hindi.settings')
 
 application = get_wsgi_application()
-
-def main():
-	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ICS_hindi.settings')
-
-if __name__ == '__main__':
-
-    from static.text_pre_processing import textVocab
-
-    main()
